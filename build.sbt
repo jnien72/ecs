7c2924df2be71deb0c219b394b585eda19218d19
johnny <a@b.com>
2018-07-07 14:15:50 +0800

    add logging configuration

--- build.sbt
+++ build.sbt
@@ -2,7 +2,14 @@ name := "ecs"
 version := "0.1"
 scalaVersion := "2.12.6"
 
-libraryDependencies ++= Seq()
+libraryDependencies ++= Seq(
+  "org.apache.logging.log4j" % "log4j-api" % "2.11.0",
+  "org.apache.logging.log4j" % "log4j-core" % "2.11.0",
+  "org.slf4j" % "slf4j-log4j12" % "1.7.25",
+  "org.http4s" %% "http4s-blaze-server" % "0.15.4a",
+  "org.http4s" %% "http4s-dsl"          % "0.15.4a",
+  "org.http4s" %% "http4s-argonaut"     % "0.15.4a"
+)
 
 updateOptions := updateOptions.value.withCachedResolution(true)
 
--- src/main/scala/com/test/ecs/EcsServer.scala
+++ src/main/scala/com/test/ecs/EcsServer.scala
@@ -1,8 +1,22 @@
 package com.test.ecs
 
+import org.http4s.HttpService
+import org.http4s.server.blaze.BlazeBuilder
+import org.http4s.dsl._
+
 object EcsServer {
-  def main(args:Array[String]):Unit={
-    println("hello")
+
+  val service = HttpService {
+    case GET -> Root / "hello" / name => {
+      Ok(s"hello ${name}!")
+    }
+    case req @ POST -> Root / "echo" => {
+      Ok(req.body)
+    }
   }
 
+  def main(args:Array[String]):Unit={
+    BlazeBuilder.bindHttp(8080, "0.0.0.0")
+      .mountService(service, "/").run.awaitShutdown()
+  }
 }
