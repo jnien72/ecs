a6f3c3d1f0dd15a3779a7796dbd7efe8ff04abcb
johnny <a@b.com>
2018-07-07 14:40:01 +0800

    add json utils

--- build.sbt
+++ build.sbt
@@ -1,11 +1,14 @@
 name := "ecs"
 version := "0.1"
-scalaVersion := "2.12.6"
+scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq(
   "org.apache.logging.log4j" % "log4j-api" % "2.11.0",
   "org.apache.logging.log4j" % "log4j-core" % "2.11.0",
   "org.slf4j" % "slf4j-log4j12" % "1.7.25",
+  "com.fasterxml.jackson.module" % "jackson-module-scala_2.11" % "2.8.4",
+  "com.fasterxml.jackson.core" % "jackson-databind" % "2.8.4",
+  "com.fasterxml.jackson.core" % "jackson-core" % "2.8.4",
   "org.http4s" %% "http4s-blaze-server" % "0.15.4a",
   "org.http4s" %% "http4s-dsl"          % "0.15.4a",
   "org.http4s" %% "http4s-argonaut"     % "0.15.4a"
---
+++ src/main/scala/com/test/ecs/util/JsonUtils.scala
@@ -0,0 +1,31 @@
+package com.test.ecs.util
+
+import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper}
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
+
+object JsonUtils {
+
+  val mapper = new ObjectMapper() with ScalaObjectMapper
+  mapper.registerModule(DefaultScalaModule)
+  mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+  def toJson(value: Map[Symbol, Any]): String = {
+    toJson(value map { case (k, v) => k.name -> v })
+  }
+
+  def toJson(value: Any): String = {
+    toJson(value, false)
+  }
+
+  def toJson(value: Any, prettyPrint: Boolean): String = {
+    if (!prettyPrint)
+      mapper.writeValueAsString(value)
+    else
+      mapper.writerWithDefaultPrettyPrinter().writeValueAsString(value)
+  }
+
+  def fromJson[T](json: String)(implicit m: Manifest[T]): T = {
+    mapper.readValue[T](json)
+  }
+}
